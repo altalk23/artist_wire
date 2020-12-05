0762e10cfdf6911502ddffad2ec0bbc10ac84ee8
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2020-12-05 19:14:08 +0300

    form stuff, didn't test

--- lib/screen/signup/form.dart
+++ lib/screen/signup/form.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:artist_project/screen/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -6,87 +8,184 @@ class SignupFormPage extends StatefulWidget {
   _SignupFormPageState createState() => _SignupFormPageState();
 }
 
-class UsernamePage extends StatefulWidget {
-  UsernamePage({Key key}) : super(key: key);
-
-  @override
-  _UsernamePageState createState() => _UsernamePageState();
-}
-
-class _UsernamePageState extends State<UsernamePage> {
+abstract class _ValueHolder {
   bool valid;
   String errorMessage;
-  String _username = '';
-  FocusNode _usernameFocusNode = FocusNode();
 
+  void validate();
+
+  void dispose();
+
+  void appear(BuildContext context);
+
+  void disappear(BuildContext context);
+}
+
+class _UsernameData extends _ValueHolder {
+  String username = '';
+  String name = '';
+  FocusNode usernameFocusNode = FocusNode();
+  FocusNode nameFocusNode = FocusNode();
+
+  @override
   void validate() {
-    //TODO: username validation
+    // TODO: username and name validation
     valid = true;
   }
 
   @override
   void dispose() {
-    _usernameFocusNode.dispose();
-    super.dispose();
-  }
-
-  void appear(context) {
-    FocusScope.of(context).requestFocus(_usernameFocusNode);
-  }
-
-  void disappear(context) {
-    FocusScope.of(context).unfocus();
+    usernameFocusNode.dispose();
+    nameFocusNode.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(labelText: 'Kullanıcı Adı'),
-      keyboardType: TextInputType.text,
+  void appear(context) {
+    FocusScope.of(context).requestFocus(usernameFocusNode);
+  }
 
-      // Step 3 - Fetch User Input
-      onChanged: (value) {
-        setState(() {
-          _email = value;
-        });
-      },
-
-      // Step 4 - Validate User Input
-      onSubmitted: (value) {
-        _validateEmail();
-
-        // Step 2 - Switch Between TextFields
-        if (_emailValid) {
-          _switchInputField(_formIndex + 1);
-        }
-      },
-
-      // Step 2 - Switch Between TextFields
-      focusNode: _emailFocusNode,
-    );
+  @override
+  void disappear(context) {
+    FocusScope.of(context).unfocus();
   }
 }
 
+class _GenderData extends _ValueHolder {
+  String gender;
+  List<String> genders = ['Erkek', 'Kadın', 'Diğer'];
+  FocusNode genderFocusNode = FocusNode();
 
+  @override
+  void validate() {
+    // TODO: gender validation
+    valid = true;
+  }
+
+  @override
+  void dispose() {
+    genderFocusNode.dispose();
+  }
+
+  @override
+  void appear(context) {
+    FocusScope.of(context).requestFocus(genderFocusNode);
+  }
+
+  @override
+  void disappear(context) {
+    FocusScope.of(context).unfocus();
+  }
+}
+
+class _QualitiesData extends _ValueHolder {
+  List<String> qualities = [];
+  List<String> allQualities = ['Yo', 'Mama', 'So', 'Fat', 'She', 'Had', 'To', 'Split', 'This', 'To', 'Fit'];
+  FocusNode qualitiesFocusNode = FocusNode();
+
+  @override
+  void validate() {
+    // TODO: quality validation
+    valid = true;
+  }
+
+  @override
+  void dispose() {
+    qualitiesFocusNode.dispose();
+  }
+
+  @override
+  void appear(context) {
+    FocusScope.of(context).requestFocus(qualitiesFocusNode);
+  }
+
+  @override
+  void disappear(context) {
+    FocusScope.of(context).unfocus();
+  }
+}
+
+class _PhotoData extends _ValueHolder {
+  File image;
+  final picker = null;
+
+  @override
+  void validate() {
+    // TODO: photo validation
+    valid = true;
+  }
+
+  @override
+  void dispose() {
+  }
+
+  @override
+  void appear(context) {
+  }
+
+  @override
+  void disappear(context) {
+    FocusScope.of(context).unfocus();
+  }
+}
+
+class _DetailData extends _ValueHolder {
+  String bio = '';
+  String location = '';
+  FocusNode bioFocusNode = FocusNode();
+  FocusNode locationFocusNode = FocusNode();
+
+  @override
+  void validate() {
+    // TODO: bio and location validation
+    valid = true;
+  }
+
+  @override
+  void dispose() {
+    bioFocusNode.dispose();
+    locationFocusNode.dispose();
+  }
+
+  @override
+  void appear(context) {
+    FocusScope.of(context).requestFocus(bioFocusNode);
+  }
+
+  @override
+  void disappear(context) {
+    FocusScope.of(context).unfocus();
+  }
+}
 
 class _SignupFormPageState extends State<SignupFormPage> {
-  var _username = GlobalKey();
-  var _name = _ValueHolder();
-  var _surname = _ValueHolder();
+  var _name = _UsernameData();
+  var _gender = _GenderData();
+  var _qualities = _QualitiesData();
+  var _photo = _PhotoData();
+  var _detail = _DetailData();
   List<_ValueHolder> _order;
   int _formIndex = 0;
   String _errorMessage = '';
 
   @override
   void initState() {
-    _usernameKey.
-    _order = [_username, _name, _surname];
+    _order = [_name, _gender, _qualities, _photo, _detail];
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _order.forEach((element) {
+      element.dispose();
+    });
+    super.dispose();
   }
 
   void switchInputFields(int newIndex) {
     setState(() {
+      _order[_formIndex].disappear(context);
       _formIndex = newIndex;
+      _order[_formIndex].appear(context);
     });
   }
 
@@ -99,7 +198,127 @@ class _SignupFormPageState extends State<SignupFormPage> {
         children: <Widget>[
           IndexedStack(
             index: _formIndex,
-            children: _order.map((e) => e.widget(context)),
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Kullanıcı Adı'),
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      onChanged: (value) {
+                        setState(() {
+                          _name.username = value;
+                        });
+                      },
+                      onSubmitted: (value) {
+                        FocusScope.of(context).nextFocus();
+                      },
+                      focusNode: _name.usernameFocusNode,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Sanatçı Adı'),
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      onChanged: (value) {
+                        setState(() {
+                          _name.name = value;
+                        });
+                      },
+                      onSubmitted: (value) {
+                        FocusScope.of(context).unfocus();
+                      },
+                      focusNode: _name.nameFocusNode,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: DropdownButton<String>(
+                  hint: Text('Bir cinsiyet seçiniz'),
+                  value: _gender.gender,
+                  icon: Icon(Icons.arrow_downward),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      _gender.gender = newValue;
+                    });
+                  },
+                  items: _gender.genders
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    ListView(
+                      children: _qualities.qualities.map((e) => Text(e)).toList(),
+                    ),
+                    DropdownButton<String>(
+                      hint: Text('Bir özellik seçiniz'),
+                      value: null,
+                      icon: Icon(Icons.arrow_downward),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          _qualities.qualities.add(newValue);
+                        });
+                      },
+                      items: _qualities.allQualities
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text('Bir resim seçiniz'),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Kullanıcı Adı'),
+                      keyboardType: TextInputType.multiline,
+                      textInputAction: TextInputAction.next,
+                      onChanged: (value) {
+                        setState(() {
+                          _detail.bio = value;
+                        });
+                      },
+                      onSubmitted: (value) {
+                        FocusScope.of(context).nextFocus();
+                      },
+                      focusNode: _detail.bioFocusNode,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Sanatçı Adı'),
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      onChanged: (value) {
+                        setState(() {
+                          _detail.location = value;
+                        });
+                      },
+                      onSubmitted: (value) {
+                        FocusScope.of(context).unfocus();
+                      },
+                      focusNode: _detail.locationFocusNode,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Text(_errorMessage),
           Row(
@@ -121,7 +340,7 @@ class _SignupFormPageState extends State<SignupFormPage> {
                   });
                   if (_order[_formIndex].valid) {
                     if (_formIndex == _order.length - 1) {
-                      // submit
+                      // TODO: submit
                     }
                     switchInputFields(_formIndex + 1);
                   }
